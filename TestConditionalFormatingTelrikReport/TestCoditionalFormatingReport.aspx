154e48949fa1b4c7fac76b8de3f6f0413f1ffab7
Navin sipl <navin_sipl@systematixindia.com>
2013-11-14 09:47:35 +0530

    new files

---
+++ TestConditionalFormatingTelrikReport/TestCoditionalFormatingReport.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="TestCoditionalFormatingReport.aspx.cs" Inherits="TestCoditionalFormatingReport" %>
+
+<%@ Register Assembly="Telerik.ReportViewer.WebForms, Version=7.2.13.1016, Culture=neutral, PublicKeyToken=a9d7983dfcc261be"
+    Namespace="Telerik.ReportViewer.WebForms" TagPrefix="telerik" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+       <telerik:ReportViewer ID="ReportViewer1" runat="server"></telerik:ReportViewer>
+    </div>
+    </form>
+</body>
+</html>
---
+++ TestConditionalFormatingTelrikReport/TestCoditionalFormatingReport.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CoditionalFormating;
+
+public partial class TestCoditionalFormatingReport : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            //Here create the object of our report class and assing it to the "ReportSource" property of "ReportViewer" control.
+            ReportWithConditionalFomating pd = new ReportWithConditionalFomating();
+            Telerik.Reporting.InstanceReportSource inst = new Telerik.Reporting.InstanceReportSource();
+            inst.ReportDocument = pd;
+            this.ReportViewer1.ReportSource = pd;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+}
